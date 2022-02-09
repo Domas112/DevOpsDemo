45dcb00ccdaadfee5356f729d315e72c4dba29e4
Domas112 <43904920+Domas112@users.noreply.github.com>
2022-02-09 22:13:51 +0200

    Delete TodosDatabase directory

--- TodosDatabase/dbo/Tables/Todos.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Todos]
-(
-	[Id] uniqueidentifier NOT NULL PRIMARY KEY,
-	[Description] varchar(60) NOT NULL,
-	[CreatedAt] datetime NOT NULL,
-	[Deadline] datetime NOT NULL
-
-
-)
